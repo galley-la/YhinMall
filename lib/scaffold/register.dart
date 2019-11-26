c9200f5552f8b4110ce7e52d1562fa8455d9ca87
Hanuman <shanin.har@rmutr.ac.th>
2019-11-26 08:58:00 +0700

    Start 26 Nov

---
+++ lib/scaffold/register.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:yhinmall/utility/my_style.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  // Field
+  String name, email, password;
+  final formKey = GlobalKey<FormState>();
+
+  // Method
+  Widget nameForm() {
+    Color color = Colors.redAccent;
+    return TextFormField(
+      onSaved: (String string) {
+        name = string.trim();
+      },
+      decoration: InputDecoration(
+        hintText: 'English Only',
+        helperText: 'Please Type Your Name Blank',
+        helperStyle: TextStyle(color: color),
+        labelText: 'Display Name',
+        labelStyle: TextStyle(color: color),
+        icon: Icon(
+          Icons.account_box,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Widget emailForm() {
+    Color color = Colors.cyan;
+    return TextFormField(
+      onSaved: (String string) {
+        email = string.trim();
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@mail.com',
+        helperText: 'Please Type Your E-mail in Blank',
+        helperStyle: TextStyle(color: color),
+        labelText: 'Email',
+        labelStyle: TextStyle(color: color),
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Widget passwordForm() {
+    Color color = Colors.greenAccent;
+    return TextFormField(
+      onSaved: (String string) {
+        password = string.trim();
+      },
+      decoration: InputDecoration(
+        hintText: 'More 6 Charactor',
+        helperText: 'Please Type Your Password in Blank',
+        helperStyle: TextStyle(color: color),
+        labelText: 'Password',
+        labelStyle: TextStyle(color: color),
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        formKey.currentState.save();
+        print('name = $name, email = $email, password = $password');
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyStyle().textColor,
+        actions: <Widget>[
+          registerButton(),
+        ],
+        title: Text('Register'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+              colors: [Colors.white, MyStyle().mainColor],
+              radius: 1.2,
+              center: AlignmentDirectional(0, -0.5)),
+        ),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            padding: EdgeInsets.all(40.0),
+            children: <Widget>[
+              nameForm(),
+              emailForm(),
+              passwordForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
